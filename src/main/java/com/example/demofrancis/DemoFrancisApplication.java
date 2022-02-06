2f1f86ed6779be18ea342ad03a693fd856eb249f
Julian Lafratta <julianl@magnetico.dev>
2022-02-06 20:59:32 -0300

    base project files

---
+++ src/main/java/com/example/demofrancis/DemoFrancisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofrancis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFrancisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFrancisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demofrancis/DemoFrancisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofrancis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFrancisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
